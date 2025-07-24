8ff79d5944f78e6028a2236cc37445fdc2fda434
tatsianabrehova <tatsianabrehova@gmail.com>
2025-07-24 18:04:35 +0300

    banking app gui

--- src/guis/AppLauncher.java
+++ src/guis/AppLauncher.java
@@ -1,7 +1,9 @@
 package guis;
 
+import db_objs.User;
 import guis.LoginGui;
 import javax.swing.*;
+import java.math.BigDecimal;
 
 public class AppLauncher {
     public static void main(String[] args){
@@ -9,7 +11,8 @@ public class AppLauncher {
             @Override
             public void run() {
                 //new LoginGui().setVisible(true);
-                new RegisterGui().setVisible(true);
+                //new RegisterGui().setVisible(true);
+            new BankingAppGui(new User(1,"username","password",new BigDecimal("20.00"))).setVisible(true);
             }
         });
     }
--- src/guis/BankingAppGui.java
+++ src/guis/BankingAppGui.java
@@ -1,4 +1,29 @@
 package guis;
 
-public class BankingAppGui {
+import db_objs.User;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BankingAppGui extends BaseFrame {
+
+
+    public BankingAppGui(User user){
+        super("Banking App",user);
+    }
+    @Override
+    protected void addGuiComponents() {
+String welcomeMessage="<html>"+"<body style='text-align:center'>"+"<b>Hello"+user.getUsername()+"</b><br>"+"What would you like to do today?</body></html>";
+        JLabel welcomeMessageLabel= new JLabel(welcomeMessage);
+        welcomeMessageLabel.setBounds(0,20,getWidth()-10,40);
+        welcomeMessageLabel.setFont(new Font("Dialog", Font.PLAIN,16));
+        welcomeMessageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(welcomeMessageLabel);
+
+        JLabel currentBalanceLabel= new JLabel("Current Balance");
+        welcomeMessageLabel.setBounds(0,20,getWidth()-10,40);
+        welcomeMessageLabel.setFont(new Font("Dialog", Font.PLAIN, 16));
+
+
+    }
 }
